59b21c8173fce4939d0752638cb1641297b0a929
wyicwx <wyicwx@gmail.com>
2015-10-23 15:42:18 +0800

    release 0.0.6

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ var path = require('path');
 var AKOStream = require('AKOStream');
 var origin = AKOStream.origin;
 
-function comp(mod, context) {
-	var bone = require('bone');
-	if(!cache[mod]) {	
+function comp(mod, context, vfs) {
+	if(!cache[mod]) {
 		context || (context = {});
 
 		var bonefileStat = null;
@@ -18,12 +17,12 @@ function comp(mod, context) {
 		var module = cache[mod] = rewire(mod);
 		var bonefs = _.clone(fs);
 		bonefs.readFile = function(file, encoding, callback) {
-			if(bone.fs.existFile(file, {notFs: true})) {
-				file = bone.fs.pathResolve(file);
+			if(vfs.existFile(file, {notFs: true})) {
+				file = vfs.pathResolve(file);
 				if(cachedFile[file]) {
 					callback(null, buffer);
 				} else {				
-					AKOStream.aggreStream(bone.fs.createReadStream(file, encoding)).on('data', function(buffer) {
+					AKOStream.aggreStream(vfs.createReadStream(file, encoding)).on('data', function(buffer) {
 						callback(null, buffer);
 					});
 				}
@@ -32,33 +31,33 @@ function comp(mod, context) {
 			}
 		};
 		bonefs.createReadStream = function(file) {
-			file = bone.fs.pathResolve(file);
+			file = vfs.pathResolve(file);
 			if(cachedFile[file]) {
 				return origin(cachedFile[file]);
 			} else {
 				var args = _.toArray(arguments);
-				return bone.fs.createReadStream.apply(bone.fs, args);
+				return vfs.createReadStream.apply(vfs, args);
 			}
 		};
 		bonefs.readdir = function(p, callback) {
-			var result = bone.fs.readDir(p);
+			var result = vfs.readDir(p);
 			callback(null, result);
 		};
 		bonefs.stat = function(file, callback) {
-			file = bone.fs.pathResolve(file);
-			var isFile = bone.fs.existFile(file, {notFs: true});
-			var isDir = bone.fs.search(file, {notFs: true}).length > 0;
+			file = vfs.pathResolve(file);
+			var isFile = vfs.existFile(file, {notFs: true});
+			var isDir = vfs.search(file, {notFs: true}).length > 0;
 			var args = _.toArray(arguments);
 			var dir = isFile ? path.dirname(file) : file;
 
 			if(isFile) {
 				if(!bonefileStat) {
-					fs.stat(path.join(bone.fs.base, 'bonefile.js'), function(err, stat) {
+					fs.stat(path.join(vfs.base, 'bonefile.js'), function(err, stat) {
 						bonefileStat = stat;
 						bonefs.stat(file, callback);
 					});
 				} else {
-					AKOStream.aggre(bone.fs.createReadStream(file)).on('data', function(buffer) {
+					AKOStream.aggre(vfs.createReadStream(file)).on('data', function(buffer) {
 						// todo cache buffer  
 						cachedFile[file] = buffer;
 						setTimeout(function() {
@@ -75,7 +74,7 @@ function comp(mod, context) {
 				}
 			} else if(isDir) {
 				if(!bonebaseStat) {
-					fs.stat(bone.fs.base, function(err, stat) {
+					fs.stat(vfs.base, function(err, stat) {
 						bonebaseStat = stat;
 						bonefs.stat(file, callback);
 					});
